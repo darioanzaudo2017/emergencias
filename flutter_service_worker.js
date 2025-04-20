46dfa23a96188b94bf15da3dcb2180d1b3a34fd7
darioanzaudo2017 <darioanzaudo@gmail.com>
2025-04-20 13:52:42 -0300

    kkk

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6253e2224322d655fdf72894da4f82e0
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "56e86d54a563497d6bb0128e22143dd9",
 "/": "56e86d54a563497d6bb0128e22143dd9",
-"main.dart.js": "19820d65f2ad2cac84f05a460ee8b10b",
+"main.dart.js": "5f22c823a60a092b593134a17e7f7a87",
 "version.json": "d731583de6cdc736163f106629db1f40"};
 // The application shell files that are downloaded before a service worker can
 // start.
